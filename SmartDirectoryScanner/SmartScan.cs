de4f1e6cf2a69f03cbc3dd294b492e09c1829d4f
Gayane Khachatryan <kh.gaiane@gmail.com>
2017-01-29 19:11:47 +0400

    Added project to repository.

---
+++ SmartDirectoryScanner/SmartScan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartDirectoryScanner
+{
+    public static class SmartScan
+    {
+        public static string ScanDirectory(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                throw new DirectoryNotFoundException("Directory doesn't exists!");
+            }
+
+            var directoryBuilder = new StringBuilder();
+            var directoryInfo = new DirectoryInfo(directoryPath);
+            directoryBuilder.Append(directoryInfo.Name);
+            var directoryChildren = directoryInfo.GetDirectories();
+            if (directoryChildren.Length >= 0)
+            {
+                directoryBuilder.Append("\n\t\\");
+                foreach (var childDirectory in directoryChildren)
+                {
+                    directoryBuilder.Append(ScanDirectory(childDirectory.FullName));
+                    directoryBuilder.Append("\n\t\t\\");
+                }
+            }
+
+            return directoryBuilder.ToString();
+        }
+    }
+}
